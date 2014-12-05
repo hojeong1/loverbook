bfe443d3f1310e612c744035e73482b39d2287c1
Shinji PARK <uuooiip@naver.com>
2014-12-05 15:03:49 +0900

    메뉴 입력받는 부분 수정되었습니다.

--- loverbook.java
+++ loverbook.java
@@ -1,12 +1,18 @@
 package loverbook;
 
+import java.io.IOException;
+
 public class loverbook {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("<홈 메뉴>");
 		System.out.println("1. 학생");
 		System.out.println("2. 사서");
 		System.out.println("권한을 선택하시오 : ");
+		int menu1 = System.in.read();
+		
+		System.out.println("입력받은 메뉴는 "+ menu1 + "이다.");
+		
 		
 
 	}
