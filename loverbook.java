7781be08ad75dbcddf9a64f8aa3dbf4981842ede
Shinji PARK <uuooiip@naver.com>
2014-12-05 14:44:27 +0900

    This is first java file.

---
+++ loverbook.java
@@ -0,0 +1,14 @@
+package loverbook;
+
+public class loverbook {
+
+	public static void main(String[] args) {
+		System.out.println("<홈 메뉴>");
+		System.out.println("1. 학생");
+		System.out.println("2. 사서");
+		System.out.println("권한을 선택하시오 : ");
+		
+
+	}
+
+}
