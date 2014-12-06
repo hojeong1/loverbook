b2ecdadfb8006d9860c55272d8c763ca349692bb
hojeong1 <hj106lee@naver.com>
2014-12-06 13:38:37 +0900

    세 번째 파일입니다.

---
+++ loverbook.java
@@ -0,0 +1,25 @@
+package loverbook;
+
+import java.io.IOException;
+
+public class loverbook {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("<홈 메뉴>");
+		System.out.println("1. 학생");
+		System.out.println("2. 사서");
+		System.out.println("권한을 선택하시오 : ");
+		int menu1 = System.in.read();
+		
+		System.out.println("입력받은 메뉴는 "+ menu1 + "이다.");
+		
+		System.out.println("git hub test");
+		System.out.println("contributor : Shinji PARK");
+		
+		System.out.println("두번째 수정 해보겠습니다.");
+		System.out.println("세번째 수정 해보겠습니다.");
+		System.out.println("두번째 수정 해보겠습니다.");
+
+	}
+
+}
