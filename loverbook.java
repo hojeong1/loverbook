36e80c401b3af7b1f26beb34bb87f0de0705680d
Shinji PARK <sjpark013@naver.com>
2014-12-07 10:44:37 +0900

    librarian 메뉴부분 구현

--- loverbook.java
+++ loverbook.java
@@ -1,28 +1,36 @@
 package loverbook;
 
-import java.io.IOException;
+import java.util.Scanner;
+
 
 public class loverbook {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		
 		System.out.println("<홈 메뉴>");
 		System.out.println("1. 학생");
 		System.out.println("2. 사서");
 		System.out.println("권한을 선택하시오 : ");
-		int menu1 = System.in.read();
+		int h_menu = s.nextInt();
 		
-		System.out.println("입력받은 메뉴는 "+ menu1 + "이다.");
+		System.out.println("입력받은 메뉴는 "+ h_menu + "이다.");
 		
 		System.out.println("git hub test");
 		
-		System.out.println("두번째 수정 해보겠습니다.");
-		System.out.println("세번째 수정 해보겠습니다.");
-
-		System.out.println("다섯번째 수정 해보겠습니다.");
-
-		System.out.println("수정 끄읕");
-
-
+		if(h_menu == 1){
+			
+		}
+		
+		else{//librarian
+			System.out.println("<사서 메뉴>");
+			System.out.println("1. 도서 등록");
+			System.out.println("2. 도서 정보 수정");
+			System.out.println("3. 도서 삭제");
+			System.out.println("항목을 선택하시오 : ");
+			int l_menu = s.nextInt();
+		}
+		
 	}
 
 }
