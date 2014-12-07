e082db4b07613668aad9cf16462f07c7304bcba5
haerimji <jigongju04@naver.com>
2014-12-07 15:26:31 +0900

    메뉴 추가했습니다.

--- loverbook.java
+++ loverbook.java
@@ -7,10 +7,7 @@ public class loverbook {
 
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-<<<<<<< HEAD
-=======
 		int l_menu=0;
->>>>>>> librarian
 		
 		System.out.println("<홈 메뉴>");
 		System.out.println("1. 학생");
@@ -24,30 +21,36 @@ public class loverbook {
 		System.out.println("git hub test");
 		
 		if(h_menu == 1){
+			while (l_menu != 3)
+			{
+			System.out.println("<학생 메뉴>");
+			System.out.println("1. 도서 검색 및 대출");
+			System.out.println("2. 내가 빌린 도서 목록 확인");
+			System.out.println("3. 종료");
+			System.out.println("4");
+			System.out.println("5");
+			System.out.println("5");
+			System.out.println("5");
+			System.out.println("수정해봐");
+			System.out.println("항목을 선택하시오 : ");
+			l_menu = s.nextInt();
+			}
 			
 		}
 		
 		else{//librarian
-<<<<<<< HEAD
-=======
 			while (l_menu != 4)
 			{
->>>>>>> librarian
 			System.out.println("<사서 메뉴>");
 			System.out.println("1. 도서 등록");
 			System.out.println("2. 도서 정보 수정");
 			System.out.println("3. 도서 삭제");
-<<<<<<< HEAD
-			System.out.println("항목을 선택하시오 : ");
-			int l_menu = s.nextInt();
-=======
 			System.out.println("4. 종료");
 			System.out.println("항목을 선택하시오 : ");
 			l_menu = s.nextInt();
 			}
 			
 			
->>>>>>> librarian
 		}
 		
 	}
