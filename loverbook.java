a2a66b4afde5ce2316db8aff01cdbbe99ba2aa69
haerimji <jigongju04@naver.com>
2014-12-07 14:44:56 +0900

    메뉴 종료추가했습니다.

--- loverbook.java
+++ loverbook.java
@@ -7,11 +7,13 @@ public class loverbook {
 
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
+		int l_menu=0;
 		
 		System.out.println("<홈 메뉴>");
 		System.out.println("1. 학생");
 		System.out.println("2. 사서");
 		System.out.println("권한을 선택하시오 : ");
+		System.out.println("아무거낳ㅎㅎㅎ");
 		int h_menu = s.nextInt();
 		
 		System.out.println("입력받은 메뉴는 "+ h_menu + "이다.");
@@ -23,12 +25,18 @@ public class loverbook {
 		}
 		
 		else{//librarian
+			while (l_menu != 4)
+			{
 			System.out.println("<사서 메뉴>");
 			System.out.println("1. 도서 등록");
 			System.out.println("2. 도서 정보 수정");
 			System.out.println("3. 도서 삭제");
+			System.out.println("4. 종료");
 			System.out.println("항목을 선택하시오 : ");
-			int l_menu = s.nextInt();
+			l_menu = s.nextInt();
+			}
+			
+			
 		}
 		
 	}
